2b653e81f30180ffc199952d410b9bb938728f3a
Noelia <perezgonzaleznoelia08@gmail.com>
2024-02-17 13:53:32 +0100

    movidas tareas entregadas, tarea 10 terminada

--- jquery/tarea10js/jq/eventos.js
+++ jquery/tarea10js/jq/eventos.js
@@ -1,3 +1,21 @@
 $(document).ready(function () {
-    
+    $(".texto").css("display", "none");
+
+    $("article > div > section > div").on({
+        click: function () {
+            if($(this).siblings(".texto").css("display") === "none"){
+                $(this).siblings(".texto").fadeIn(500);
+                $(this).children("div").children("svg:nth-child(2)").css({
+                    transform: "rotate(180deg)",
+                    transition: "transform 0.5s ease"
+                });
+            }else{
+                $(this).siblings(".texto").fadeOut(500);
+                $(this).children("div").children("svg:nth-child(2)").css({
+                    transform: "rotate(90deg)",
+                    transition: "transform 0.5s ease"
+                });
+            }
+        }
+    })
 });
