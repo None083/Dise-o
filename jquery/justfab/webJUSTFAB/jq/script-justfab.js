75257825846b6a5a17b709427780b4e2818d0942
Noelia <perezgonzaleznoelia08@gmail.com>
2024-02-20 00:39:12 +0100

    Queda último punto de la primera parte de justfab

--- jquery/justfab/webJUSTFAB/jq/script-justfab.js
+++ jquery/justfab/webJUSTFAB/jq/script-justfab.js
@@ -1,19 +1,55 @@
 $(document).ready(function () {
     $("header > div > nav#menu-principal > ul#menu").css("display", "none");
+
     $("header#top > div > nav#menu-principal > span").on({
         click: function () {
             if ($("nav#menu-principal > ul#menu").css("display") === "none") {
-                $("header#top > div > nav#menu-principal > ul#menu").fadeIn(200);
+                $("header#top > div > nav#menu-principal > ul#menu").fadeIn();
             } else {
-                $("header > div > nav#menu-principal > ul#menu").fadeOut(200);
+                $("header > div > nav#menu-principal > ul#menu").fadeOut();
             }
         }
     });
-    $(window).scroll(function () {
-        if ($(document).scrollTop()) {
-            $("header#top").css("position", "fixed");
-        } else {
-            $("header#top")
+    $(window).on({
+        scroll: function () {
+            if ($(document).scrollTop()) {
+                $("header#top > div").addClass("fijado");
+            } else {
+                $("header#top > div").removeClass("fijado");
+            }
+            if ($(this).scrollTop() > 500) {
+                $("div#volverarriba").fadeIn();
+            } else {
+                $("div#volverarriba").fadeOut();
+            }
         }
     });
+
+    $("div#volverarriba").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 800);
+        return false;
+    });
+
+    $("nav#menu-principal > ul#menu > li").on({
+        click: function () {
+            if($(this).children("ul").css("display") === "none"){
+                $("nav#menu-principal > ul#menu > li > ul").slideUp(500);
+                $("a > span").css({
+                    transform: "rotate(0deg)",
+                    transition: "transform 0.5s ease"
+                })
+                $(this).children("ul").slideDown(500);
+                $(this).find("a > span").css({
+                    transform: "rotate(-180deg)",
+                    transition: "transform 0.5s ease"
+                })
+            }else{
+                $(this).children("ul").slideUp(500);
+                $(this).find("a > span").css({
+                    transform: "rotate(0deg)",
+                    transition: "transform 0.5s ease"
+                })
+            }
+        }
+    })
 })
