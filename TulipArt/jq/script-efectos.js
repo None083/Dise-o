5f9923a7a0eaf3554990b997598fec69ba929df3
Noelia <perezgonzaleznoelia08@gmail.com>
2024-05-21 16:47:11 +0200

    algunos arreglillos en tulipart

--- TulipArt/jq/script-efectos.js
+++ TulipArt/jq/script-efectos.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
 
+    // Inicio header
     $(window).on('resize', function () {
-        $("ul#menu-desplegado").fadeOut(300);
+        $("ul#menu-desplegado").fadeOut();
+        $("ul#menu-iconos-adicional").fadeOut();
+        $('nav.menu-post').fadeOut();
+        $("nav#capa-buscador").fadeOut();
     });
 
     $(window).on('scroll', function () {
@@ -10,6 +14,8 @@ $(document).ready(function () {
             $('header.fixed').stop().animate({ opacity: 0.8 }, 500);
             $("ul#menu-desplegado").fadeOut(1);
             $('nav.menu-post').fadeOut();
+            $("ul#menu-iconos-adicional").fadeOut(1);
+            $("nav#capa-buscador").fadeOut();
         } else {
             $('header.fixed').stop().animate({ opacity: 1 }, 500);
         }
@@ -24,15 +30,15 @@ $(document).ready(function () {
         }
     });
 
-    $('img#volver-arriba').on("click", function () {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 400);
-    });
-
     $("img#menu-hamb").on(
         "click", function () {
             $("ul#menu-desplegado").slideToggle(300);
+        }
+    );
+
+    $("img#menu-albondigas").on(
+        "click", function () {
+            $("ul#menu-iconos-adicional").slideToggle(300);
         }
     );
 
@@ -45,8 +51,24 @@ $(document).ready(function () {
         }
     });
 
+    $("div#search>img#lupa-blanca").on(
+        "click", function () {
+            $("nav#capa-buscador").fadeToggle();
+        }
+    );
+
+    $("nav#capa-buscador>img#equis").on(
+        "click", function () {
+            $("nav#capa-buscador").fadeOut();
+        }
+    );
+
+    // Fin header
+
+    // Hover sobre imagenes del index y profile, modo escritorio
     $(window).resize(function () {
         if ($(window).width() > 1024) {
+            // Modo escritorio
             $("article.post").on({
                 mouseenter: function () {
                     $(this).children("div.capa-post").stop().fadeIn();
@@ -54,21 +76,29 @@ $(document).ready(function () {
                 },
                 mouseleave: function () {
                     $(this).children("div.capa-post").stop().fadeOut();
-                    $(this).children("div.post-info, ul.icons-post").stop().css("display", "flex").fadeOut();
-                    $(this).children('nav.menu-post').stop().fadeOut();
+                    $(this).children("div.post-info, ul.icons-post").stop().fadeOut();
                 }
             });
+
+            // Ocultar los elementos al cambiar a modo escritorio
+            $("article.post").children("div.capa-post, div.post-info, ul.icons-post").stop().hide();
         } else {
+            // Modo móvil
             $("article.post").off("mouseenter mouseleave");
+
+            // Mostrar los elementos de forma permanente en modo móvil
+            $("main#index>section#galery-index>article.post").children("div.post-info, ul.icons-post").stop().css("display", "flex").show();
         }
     }).resize();
 
+    // Desplegar mini-menu de post en escritorio
     $("img.icon-albondiga").on(
         "click", function () {
             $(this).closest('article').children('nav.menu-post').fadeToggle(200);
         }
     );
 
+    // Cambiar estado del botón follow a following, index y profile
     $("span.boton-seguir").on("click", function () {
         $(this).fadeOut(function () {
             $(this).css({
@@ -78,6 +108,7 @@ $(document).ready(function () {
         });
     });
 
+    // Cambiar estado del like, cambio a corazón relleno, index
     $("main#index>section#galery-index>article.post>ul.icons-post>li:first-child>img").on("click", function () {
         var img = $(this);
         if (img.attr('src') === "imagenes/icons/Favorite.svg") {
@@ -93,6 +124,7 @@ $(document).ready(function () {
         }
     });
 
+    // Cambiar estado del like, cambio a corazón relleno, profile y post
     $("main#profile-page>section#profile-gallery>article.post>ul.icons-post>li:first-child>img, main.post>section.post>div#option>div#izq>img:first-child").on("click", function () {
         var img = $(this);
         if (img.attr('src') === "../imagenes/icons/Favorite.svg") {
@@ -108,6 +140,7 @@ $(document).ready(function () {
         }
     });
 
+    // Validar login, error de campo vacío
     $("main#login>div.containForm>form>button").on("click", function () {
 
         if ($("input#username").val() === "") {
@@ -125,18 +158,28 @@ $(document).ready(function () {
         }
     });
 
+    // Botón de volver arriba
+    $('img#volver-arriba').on("click", function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 400);
+    });
+
+    // Cerrar el vídeo de promo en index
     $("div#video-promo>img").on(
         "click", function () {
             $("div#video-promo").fadeOut(200);
         }
     );
 
+    // Cerrar el banner de loguearse en index
     $("div#joinOrLogin>img#equisjoin").on(
         "click", function () {
             $("div#joinOrLogin").fadeOut(200);
         }
     );
 
+    // Aceptar las cookies en la página de cookies
     $("span.btn-cookies").on(
         "click", function () {
             $("div#accept-conditions").fadeOut(200);
